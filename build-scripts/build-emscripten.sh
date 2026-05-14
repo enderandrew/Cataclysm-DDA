f2032f5c1450db547a97683e8c7f04b04ecdeb1d
T. J. Brumfield <enderandrew@gmail.com>
2026-05-13 23:09:00 -0500

    Update build-emscripten.sh

--- build-scripts/build-emscripten.sh
+++ build-scripts/build-emscripten.sh
@@ -3,11 +3,11 @@ set -exo pipefail
 
 CCACHE=${CCACHE:-0}
 
-emsdk install 3.1.51
-emsdk activate 3.1.51
+emsdk install latest
+emsdk activate latest
 if [ "$CCACHE" == "1" ]
 then
     emsdk activate ccache-git-emscripten-64bit
 fi
 
-make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 CCACHE="$CCACHE" LINTJSON=0 cataclysm-tiles.js
+emmake make -j8 CLANG=1 NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 CCACHE="$CCACHE" LINTJSON=0 cataclysm-tiles.js
